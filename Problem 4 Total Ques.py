91f041c5897a34d13a37eccd86e0fdb170c2ecf6
Ratan Raj <ratanojha10@gmail.com>
2019-08-29 10:20:55 +0530

    Update Problem 4 Total Ques.py

--- Problem 4 Total Ques.py
+++ Problem 4 Total Ques.py
@@ -1,21 +1,30 @@
 #Error
 
-
 num=int(input("Input test cases: "))
 
 for i in range(num):
     stg=input("Enter String: ")
-    if '???' in stg:
-        for j in range(len(stg)):
-            if stg[j]=='?':
-                if stg[j+1]=='?' and stg[j+2]=='?':
-                    try:
-                        if int(stg[j-1])+int(stg[j+3])==10:
-                            print("True")
-                            break
-                    except:
-                        print("False")
-    elif '???' not in stg:
-        print("True")
+    B=0
+    A="False"
+    size=len(stg)
+    for i in range(size):
+        try:
+            for j in range(size):
+                if int(stg[j])+int(stg[i])==10:
+                    for x in range(i,j+1,1):
+                        if stg[x]=='?':
+                            B+=1
+                            continue
+                        else:
+                            continue
+                            
+                else:
+                    continue
+                               
 
+        except:
+            continue
+    if B==3:
+        A="True"
+    print(B)
 
