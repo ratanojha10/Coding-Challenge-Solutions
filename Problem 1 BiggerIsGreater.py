d5f1169e9793e4a79c4fc4528827a93304da2f7d
Ratan Raj <ratanojha10@gmail.com>
2019-08-29 15:33:56 +0530

    Update Problem 1 BiggerIsGreater.py

--- Problem 1 BiggerIsGreater.py
+++ Problem 1 BiggerIsGreater.py
@@ -1,1 +1,22 @@
-
+from itertools import permutations
+case=int(input("Enter number of test cases: "))
+for i in range(case):
+    try:
+        stg=input("Enter string: ")
+        tup=tuple(stg)
+        A=permutations(stg)
+        k=[]
+        nlst=None
+        nstg=''
+        for j in list(A):
+            k+=[j]
+        k.sort()
+        for x in range(len(k)):
+            if k[x]==tup:
+                nlst=tuple(k[x+1])
+        for y in nlst:
+            nstg+=str(y)
+        print(nstg)
+    except:
+        print("No answer")
+        
