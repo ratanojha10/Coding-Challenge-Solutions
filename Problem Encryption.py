e87398836f1d4d2f1ffdd6762df0b33536f9c2db
Ratan Raj <ratanojha10@gmail.com>
2019-08-29 08:39:10 +0530

    Update Problem Encryption.py

--- Problem Encryption.py
+++ Problem Encryption.py
@@ -1,55 +1,67 @@
 stg=input("Enter string: ")
+
 nstg=""
+
 for i in stg:
+
     if i !=' ':
+
         nstg+=i
+
     else:
+
         continue
 
+
+
 size=len(nstg)
+
 row=int((size**0.5)//1)
+
 col=int(((size**0.5)//1)+1)
-if row*col>=size:
+
+
+def encryp(r,c):
     fstg=''
+
     k=0
-    for i in range(row):
-        for j in range(col):
+
+    for i in range(r):
+
+        for j in range(c):
+
             if k<=(size-1):
+
                 print(nstg[k],end='')
+
                 k+=1
+
             else:
+
                 pass
+
         print() 
 
-    for x in range(col):
+
+
+    for x in range(c):
+
         m=0
-        for y in range(row):
+
+        for y in range(r):
+
             if (m+x)<=(size-1):
+
                 fstg+=nstg[m+x]
-                m+=col
+
+                m+=c
+
         fstg+=' '
+
     print(fstg)
 
+if row*col>size:
+    encryp(row,col)
 else:
-    row+=1
-    fstg=''
-    k=0
-    for i in range(row):
-        for j in range(col):
-            if k<=(size-1):
-                print(nstg[k],end='')
-                k+=1
-            else:
-                pass
-        print() 
+    encryp(row+1,col)
 
-    for x in range(col):
-        m=0
-        for y in range(row):
-            if (m+x)<=(size-1):
-                fstg+=nstg[m+x]
-                m+=col
-        fstg+=' '
-    print(fstg)    
-    
-    
