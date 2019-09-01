0608e522a51410965b45c62ad2e0446ae247c3d6
Ratan Raj <ratanojha10@gmail.com>
2019-09-01 21:22:54 +0530

    Update Problem 4 Total Ques.py

--- Problem 4 Total Ques.py
+++ Problem 4 Total Ques.py
@@ -1,1 +1,21 @@
+s=input("Enter String: ")
+size=len(s)
+w=0
+res="False"
+for i in range(size):
+    try:
+        k=10-int(s[i])
+        for j in range(i,size):
+            if s[j]==str(k):
+                for x in s[i:j]:
+                    if x=='?':
+                        w+=1
+                        
+    except:
+        continue
+
+if w==3:
+    print("True")
+else:
+    print("False")
 
